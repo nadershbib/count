394ba3372663ea6db73854e1457ad345db8f7e9e
Nader Shbib <66152242+nadershbib@users.noreply.github.com>
2020-07-18 21:49:07 -0700

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ var reset = new Audio();
 reset.src="reset.mp3";
 
 
+function preventZoom(e) {
+  var t2 = e.timeStamp;
+  var t1 = e.currentTarget.dataset.lastTouch || t2;
+  var dt = t2 - t1;
+  var fingers = e.touches.length;
+  e.currentTarget.dataset.lastTouch = t2;
+
+  if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+  e.preventDefault();
+  e.target.click();
+}
+
 
 document.addEventListener("keyup",(event)=>{
     if(event.keyCode==175 || event.keyCode==38 ){
